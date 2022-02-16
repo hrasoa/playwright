dc108c565a9781720b0a8b326b9953ef8ea3ecb1
Haja Rasoahaingo <haja.rasoah@gmail.com>
2022-02-16 21:03:23 +0100

    Restore test

--- src/index.spec.js
+++ src/index.spec.js
@@ -3,5 +3,5 @@ const { test, expect } = require('@playwright/test');
 test('Basic test', async ({ page, baseURL }) => {
   await page.goto(baseURL + '/');
   const title = page.locator('h1');
-  await expect(title).toHaveText('Fail!');
+  await expect(title).toHaveText('Hello, World!');
 });

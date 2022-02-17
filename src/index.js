daa19b377133a944298cce1afcdc65d356fedb94
Haja Rasoahaingo <haja.rasoah@gmail.com>
2022-02-17 19:53:30 +0100

    Multi tabs

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
-const requestListener = function (req, res) {
+const requestListener = function(req, res) {
   res.writeHead(200);
-  res.end('<h1>Hello, World!</h1>');
+  switch(req.url) {
+    case '/blog':
+      res.end('<h1>Blog</h1>');
+      break;
+    default:
+      res.end('<h1>Hello, World!</h1>');
+      break;
+  }
 }
 
 const server = http.createServer(requestListener);
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,7 +1,20 @@
 const { test, expect } = require('@playwright/test');
 
-test('Basic test', async ({ page, baseURL }) => {
-  await page.goto(baseURL + '/');
-  const title = page.locator('h1');
-  await expect(title).toHaveText('Hello, World!');
+test('Basic test', async ({ context, page, baseURL }) => {
+  const home = await context.newPage();
+  const blog = await context.newPage();
+
+  const testHome = async () => {
+    await home.goto(baseURL + '/');
+    const title = home.locator('h1');
+    await expect(title).toHaveText('Hello, World!');
+  }
+
+  const testBlog = async () => {
+    await blog.goto(baseURL + '/blog');
+    const title = blog.locator('h1');
+    await expect(title).toHaveText('Blog');
+  }
+
+  await Promise.all([testHome(), testBlog()]);
 });
